1d3f2042973ef36e70134515cf26907bf3c80a0c
77ZaRR77 <banka1993@mail.ru>
2019-12-22 02:38:12 +0300

    temporar fix
    bloodmark probably still dissapear because G_FreeEntity pwns it

--- code/game/g_weapon.c
+++ code/game/g_weapon.c
@@ -102,11 +102,11 @@ qboolean CheckGauntletAttack( gentity_t *ent ) {
 	// send blood impact
 	if ( traceEnt->takedamage && traceEnt->player ) {
 		tent->s.otherEntityNum = traceEnt->s.number;
+        tent = G_TempEntity( tr.endpos, EV_MISSILE_HIT );
+        tent->s.eventParm = DirToByte( tr.plane.normal );
 		tent->s.weapon = ent->s.weapon;
         //G_FreeEntity(traceEnt); //ZaRR: simply delete player lol
     } else {
-        tent = G_TempEntity( tr.endpos, EV_MISSILE_HIT );
-        tent->s.eventParm = DirToByte( tr.plane.normal );
       G_FreeEntity(traceEnt);
 	}
 
